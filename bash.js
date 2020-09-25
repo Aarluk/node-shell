8c27bd2f8d90047c077c84243580a9c81eaf9ca0
Aarluk <provencenewyork@gmail.com>
2020-09-25 15:35:02 -0400

    saving changes up to Bonus part 4

---
+++ bash.js
@@ -0,0 +1,12 @@
+const pwd = require("./pwd.js");
+const ls = require("./ls.js")
+process.stdout.write("prompt > ");
+
+    process.stdin.on("data", (data) => {
+        const cmd = data.toString().trim();
+        if(cmd === "pwd") {
+            pwd();
+        } else if(cmd === "ls") {
+            ls();
+        }
+    });
